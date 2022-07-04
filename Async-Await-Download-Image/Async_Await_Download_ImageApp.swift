a34761a782c2178a2e3ac2cc696d89c076937b98
Simran Preet Singh <simran5590@gmail.com>
2022-07-03 19:59:58 -0700

    Initial Commit

---
+++ Async-Await-Download-Image/Async_Await_Download_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_Await_Download_ImageApp.swift
+//  Async-Await-Download-Image
+//
+//  Created by Simran Preet Narang on 2022-07-03.
+//
+
+import SwiftUI
+
+@main
+struct Async_Await_Download_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Async-Await-Download-Image/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Async-Await-Download-Image
+//
+//  Created by Simran Preet Narang on 2022-07-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
